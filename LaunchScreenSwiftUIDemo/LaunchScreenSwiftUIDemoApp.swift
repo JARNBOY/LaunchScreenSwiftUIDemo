8e4b794d073d24a713f4f7ebbea3e48f167f664f
Papon <papon.s@lawyerplusgroup.com>
2023-07-30 17:21:12 +0700

    Initial Commit

---
+++ LaunchScreenSwiftUIDemo/LaunchScreenSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenSwiftUIDemoApp.swift
+//  LaunchScreenSwiftUIDemo
+//
+//  Created by Papon Supamongkonchai on 30/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
